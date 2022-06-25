7a098bfdbe0ad591c5a0cf2075d40bbb79caa202
Adriana <adrianam754@gmail.com>
2022-06-25 15:09:31 -0300

    feat: add Token

---
+++ backend/src/helpers/jwtGenerator.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
+import { IToken } from '../interface/IToken';
+
+const options = {
+  expiresIn: '3h',
+};
+
+const SECRET = process.env.JWT_SECRET;
+
+const generateToken = (data: IToken) =>
+  jwt.sign({ data }, SECRET as string, options);
+
+export default generateToken;
