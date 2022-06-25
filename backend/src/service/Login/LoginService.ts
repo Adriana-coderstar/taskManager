7b5580f64340faf11c24ac90f848815d12e64d18
Adriana <adrianam754@gmail.com>
2022-06-25 15:14:31 -0300

    feat: add LoginService

---
+++ backend/src/service/Login/LoginService.ts
@@ -0,0 +1,34 @@
+import md5 from 'md5';
+import { User } from '@prisma/client';
+import { ILogin } from '../../interface/IUsers';
+import { ILoginUsers } from '../../interface/ILoginRepository';
+import { prismaClient } from '../../database/prismaClient';
+import generateToken from '../../helpers/jwtGenerator';
+
+class LoginService implements ILoginUsers {
+  async findUser({ email, password }: ILogin): Promise<User | any> {
+    const secretPass = md5(password);
+
+    const checkUser = await prismaClient.user.findUnique({
+      where: { email },
+    });
+
+    if (!checkUser || checkUser.password !== secretPass)
+      return new Error('Incorrect email or password');
+
+    const token = generateToken({
+      id: checkUser.id.toString(),
+      email: checkUser.email,
+    });
+
+    const userData = {
+      id: checkUser.id,
+      email: checkUser.email,
+      token: token,
+    };
+
+    return userData;
+  }
+}
+
+export default new LoginService();
