46423aa138546b6aa73631a3c26e4ad0e2ede467
Adriana <adrianam754@gmail.com>
2022-06-25 15:15:46 -0300

    feat: add routes in app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,17 @@ import 'dotenv/config';
 import Express from 'express';
 import cors from 'cors';
 
+import errorHandle from './middleware/errorHandle';
+import routerLogin from './routes/login.routes';
+import routeCreateUsers from './routes/create.routes';
+
 const app = Express();
 
 app.use(Express.json());
 app.use(cors());
+
+app.use(errorHandle);
+app.use(routerLogin);
+app.use(routeCreateUsers);
 
 export default app;
