41594b1bd250ff7fbaa28a25c1cddeb2cc39cb52
Adriana <adrianam754@gmail.com>
2022-06-26 17:10:42 -0300

    refactor: frontend - add token in request

--- frontend/src/service/request.js
+++ frontend/src/service/request.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-// const { REACT_APP_BACKEND_PORT, REACT_APP_HOSTNAME } = process.env;
-
 const api = axios.create({
   baseURL: 'http://localhost:3001',
 });
 
-export const createUsers = async (email, password) => {
-  api.post('/register', email, password);
+export const createUsers = async (email, password, token) => {
+  api.post('/register', email, password, { headers: { Authorization: token } });
 };
 
 export const requestLogin = async (email, password, token) =>
