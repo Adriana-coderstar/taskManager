36d8725dd932f47a19d01d1fa81961316f49f3f1
Adriana <adrianam754@gmail.com>
2022-07-05 23:39:24 -0300

    feat: add controller find, update and delete

--- backend/src/controller/Task/TaskContoller.ts
+++ backend/src/controller/Task/TaskContoller.ts
@@ -2,9 +2,10 @@ import { NextFunction, Request, Response } from 'express';
 import TaskService from '../../service/Task/TaskService';
 
 class TaskController {
-  create = async (req: Request, res: Response, next: NextFunction) => {
+  async create(req: Request, res: Response, next: NextFunction) {
     try {
       const {
+        id,
         task,
         status,
         tokenData: {
@@ -12,7 +13,7 @@ class TaskController {
         },
       } = req.body;
 
-      const newTask = await TaskService.create({ task, status, userId });
+      const newTask = await TaskService.create({ id, task, status, userId });
 
       return res.status(201).json(newTask);
     } catch (error) {
@@ -20,7 +21,46 @@ class TaskController {
 
       return next(error);
     }
-  };
+  }
+
+  async find(req: Request, res: Response, next: NextFunction) {
+    try {
+      const {
+        tokenData: {
+          data: { id: userId },
+        },
+      } = req.body;
+
+      const findTask = await TaskService.findAll(userId);
+      return res.status(200).json(findTask);
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id, task, status } = req.body;
+
+      const updateTask = await TaskService.updateTask({ id, status, task });
+
+      return res.status(200).json(updateTask);
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+
+      await TaskService.delete(id);
+
+      return res.status(200).json({ message: 'Task deleted' });
+    } catch (error) {
+      return next(error);
+    }
+  }
 }
 
 export default new TaskController();
