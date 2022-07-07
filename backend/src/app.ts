c44eed528497ece66b270c4d6bff03ed993ed8f4
Adriana <adrianam754@gmail.com>
2022-07-06 23:40:07 -0300

    refactor: remove the create route and add in login route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,6 @@ import cors from 'cors';
 
 import errorHandle from './middleware/errorHandle';
 import routerLogin from './routes/login/login.routes';
-import routeCreateUsers from './routes/login/create.routes';
 import routeCreateTasks from './routes/task/task.routes';
 
 const app = express();
@@ -16,7 +15,6 @@ app.use(cors());
 
 app.use(errorHandle);
 app.use(routerLogin);
-app.use(routeCreateUsers);
 app.use(routeCreateTasks);
 
 export default app;
--- backend/src/routes/login/login.routes.ts
+++ backend/src/routes/login/login.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import CreateController from '../../controller/Login/CreateController';
 import LoginController from '../../controller/Login/LoginController';
+import loginValidate from '../../middleware/loginValidate';
 
 const route = Router();
 
-route.post('/login', LoginController.find);
+route.post('/register', loginValidate, CreateController.create);
+route.post('/login', loginValidate, LoginController.find);
 
 export default route;
