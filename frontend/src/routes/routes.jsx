ff6136d4e287c1165be967bd05287e6c6ad7e658
Adriana <adrianam754@gmail.com>
2022-07-13 14:53:13 -0300

    feat: add routes in front end

---
+++ frontend/src/routes/routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from '../page/LoginPage';
+import RegisterPage from '../page/RegisterPage';
+import TaskPage from '../page/TaskPage';
+
+function LoginRoutes() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/task/:id" element={<TaskPage />} />
+    </Routes>
+  );
+}
+
+export default LoginRoutes;
