bda771cfba5765facd456a071b30503fe5f11376
Adriana <adrianam754@gmail.com>
2022-07-13 15:07:24 -0300

    feat: add components Register

---
+++ frontend/src/components/Register/Register.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import * as api from '../../service/requestLogin';
+import { Navigate } from 'react-router-dom';
+
+function Register() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [isLogged, setIsLogged] = React.useState(false);
+
+  const register = async (e) => {
+    e.preventDefault();
+    try {
+      await api.createUsers({ email, password });
+
+      const { data } = await api.requestLogin({
+        email,
+        password,
+      });
+      localStorage.setItem('createUser', JSON.stringify(data));
+
+      setIsLogged(true);
+    } catch (error) {
+      setIsLogged(false);
+    }
+  };
+
+  if (isLogged) return <Navigate to="/task" />;
+
+  return (
+    <section>
+      <form>
+        <input
+          type="email"
+          value={email}
+          placeholder="Digite seu email"
+          onChange={({ target: { value } }) => setEmail(value)}
+        />
+
+        <input
+          type="password"
+          value={password}
+          placeholder="Digite sua senha"
+          onChange={({ target: { value } }) => setPassword(value)}
+        />
+
+        <button type="button" onClick={register}>
+          Criar conta
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default Register;
