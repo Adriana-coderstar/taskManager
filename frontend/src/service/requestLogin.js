55c6b83c4231d7538c8aa915a2f343b3d15b11c4
Adriana <adrianam754@gmail.com>
2022-07-13 20:51:31 -0300

    refactor: remove token in request from createUsers

--- frontend/src/service/requestLogin.js
+++ frontend/src/service/requestLogin.js
@@ -4,8 +4,8 @@ const api = axios.create({
   baseURL: 'http://localhost:3001',
 });
 
-export const createUsers = async (email, password, token) => {
-  api.post('/register', email, password, { headers: { Authorization: token } });
+export const createUsers = async (email, password) => {
+  api.post('/register', email, password);
 };
 
 export const requestLogin = async (email, password, token) =>
