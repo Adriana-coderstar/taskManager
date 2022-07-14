1b2e13dda6fea9795bfc99012d287c5229358fb9
Adriana <adrianam754@gmail.com>
2022-07-13 21:13:51 -0300

    refactor: remove loginValidate from routes create users

--- backend/src/routes/login/login.routes.ts
+++ backend/src/routes/login/login.routes.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import CreateController from '../../controller/Login/CreateController';
 import LoginController from '../../controller/Login/LoginController';
 import loginValidate from '../../middleware/loginValidate';
+import TokenValidate from '../../middleware/tokenValidate';
 
 const route = Router();
 
-route.post('/register', loginValidate, CreateController.create);
-route.post('/login', loginValidate, LoginController.find);
+route.post('/register', CreateController.create);
+route.post('/login', loginValidate, TokenValidate, LoginController.find);
 
 export default route;
