3b1b212e5f5f4f5183f4ac0d349133ab07f32ff7
Adriana <adrianam754@gmail.com>
2022-07-16 16:50:11 -0300

    fix: remove id from route to fix bug

--- backend/src/routes/task/task.routes.ts
+++ backend/src/routes/task/task.routes.ts
@@ -6,7 +6,7 @@ const route = Router();
 
 route.post('/task/:id', TokenValidate, TaskController.create);
 route.get('/task/:id', TokenValidate, TaskController.find);
-route.put('/task/:id', TokenValidate, TaskController.update);
+route.put('/task/', TokenValidate, TaskController.update);
 route.delete('/task/:id', TokenValidate, TaskController.delete);
 
 export default route;
