df7e52347215b4762b52d1ef57c9ba5c551c3291
Adriana <adrianam754@gmail.com>
2022-07-16 16:55:48 -0300

    feat: add remove and update button functions

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,18 +1,31 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { deleteTask } from '../../service/requestTask';
 
-function Buttons({ remove, edit }) {
+function Buttons({ id, show }) {
+  const { token } = JSON.parse(localStorage.getItem('user'));
+
+  const handleRemove = async () => {
+    await deleteTask(id, token);
+  };
+
+  const handleEdit = async () => {
+    show(true);
+  };
+
   return (
     <>
-      <button onClick={edit}>Editar</button>
-      <button onClick={remove}>Deletar</button>
+      <button onClick={handleEdit}>Editar</button>
+      <button onClick={handleRemove}>Deletar</button>
     </>
   );
 }
 
 Buttons.propTypes = {
   edit: PropTypes.string,
+  id: PropTypes.number,
   remove: PropTypes.string,
+  show: PropTypes.string,
 };
 
 export default Buttons;
