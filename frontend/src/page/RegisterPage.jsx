13965f124c8ef229a53b30419c7339037284c0d2
Adriana <adrianam754@gmail.com>
2022-07-16 17:09:30 -0300

    refactor: remove Register component and implement in RegisterPage

--- frontend/src/page/RegisterPage.jsx
+++ frontend/src/page/RegisterPage.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
-import Register from '../components/Register/Register';
+import { createUsers } from '../service/requestLogin';
+import { Navigate } from 'react-router-dom';
 
 function RegisterPage() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [isLogged, setIsLogged] = React.useState(false);
+
+  const register = async (e) => {
+    e.preventDefault();
+    try {
+      await createUsers({ email, password });
+      setIsLogged(true);
+    } catch (error) {
+      setIsLogged(false);
+    }
+  };
+
+  if (isLogged) return <Navigate to={'/login'} />;
   return (
-    <div>
-      <Register />
-    </div>
+    <section>
+      <form>
+        <input
+          type="email"
+          value={email}
+          placeholder="Digite seu email"
+          onChange={({ target: { value } }) => setEmail(value)}
+        />
+
+        <input
+          type="password"
+          value={password}
+          placeholder="Digite sua senha"
+          onChange={({ target: { value } }) => setPassword(value)}
+        />
+
+        <button type="button" onClick={register}>
+          Criar conta
+        </button>
+      </form>
+    </section>
   );
 }
 
