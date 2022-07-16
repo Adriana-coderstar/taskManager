f18b87d0ebdb951ec6b1835e2c647e7416a34153
Adriana <adrianam754@gmail.com>
2022-07-16 17:07:28 -0300

    refactor: remove Login component and implement in LoginPage

--- frontend/src/page/LoginPage.jsx
+++ frontend/src/page/LoginPage.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
-import Login from '../components/Login/Login';
+import { requestLogin } from '../service/requestLogin';
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await requestLogin({
+        email,
+        password,
+      });
+      localStorage.setItem('user', JSON.stringify(data));
+      navigate(`/task/${data.id}`);
+    } catch (error) {
+      console.log(error.response.data.error);
+    }
+  };
+
   return (
-    <div>
-      <Login />
-    </div>
+    <section>
+      <form>
+        <input
+          type="email"
+          value={email}
+          placeholder="Digite seu email"
+          onChange={({ target: { value } }) => setEmail(value)}
+        />
+        <input
+          type="password"
+          value={password}
+          placeholder="Digite sua senha"
+          onChange={({ target: { value } }) => setPassword(value)}
+        />
+        <button onClick={login}>Enviar</button>
+        <button onClick={() => navigate('/register')}>Cadastrar</button>
+      </form>
+    </section>
   );
 }
 
