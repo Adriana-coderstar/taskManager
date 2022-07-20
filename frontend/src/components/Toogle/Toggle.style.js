1e9f241c862f1121f6d5e01b3aea0086c7c5f5ae
Adriana <adrianam754@gmail.com>
2022-07-20 16:05:25 -0300

    style: add style in image Sun and Moon

---
+++ frontend/src/components/Toogle/Toggle.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ToggleDarkMode = styled.div`
+  img {
+    height: 11vh;
+    position: absolute;
+    right: 450px;
+    top: 30px;
+    width: 100px;
+    z-index: 1;
+  }
+`;
