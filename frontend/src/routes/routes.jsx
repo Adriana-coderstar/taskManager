90b39bf6bef900260b20cd9538eda95409340c91
Adriana <adrianam754@gmail.com>
2022-07-20 15:59:14 -0300

    feat: add global style in routes

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import LoginPage from '../page/LoginPage';
-import RegisterPage from '../page/RegisterPage';
-import TaskPage from '../page/TaskPage';
+import LoginPage from '../page/Login/LoginPage';
+import RegisterPage from '../page/Login/RegisterPage';
+import TaskPage from '../page/Task/TaskPage';
+import GlobalStyle from '../style/GlobalStyle';
 
 function LoginRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route exact path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/task/:id" element={<TaskPage />} />
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/task/:id" element={<TaskPage />} />
+      </Routes>
+    </>
   );
 }
 
