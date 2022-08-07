46e24e97b4fa11b288087bbf5f78fe960c3b95bc
Adriana <adrianam754@gmail.com>
2022-08-07 14:18:50 -0300

    refactor: change enviroment PORT

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-app.listen(process.env.PORT || 3001, () =>
-  console.log(`Server running on port ${process.env.PORT}`),
-);
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
