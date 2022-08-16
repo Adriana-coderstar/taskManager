87e6e1036a9e3d29481753105fd8eb9855a7f18b
Adriana <adrianam754@gmail.com>
2022-08-15 21:51:23 -0300

    refactor: change pathname to dynamic

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import out from '../../img/out.svg';
 import emojis from '../../utils/emojis';
 import { useNavigate } from 'react-router-dom';
 import { Container, Welcome, ButtonOut } from './Header.style';
 
-function Header() {
+function Header({ id }) {
   const navigate = useNavigate();
 
   const { pathname } = window.location;
 
+  console.log(pathname);
   const emoji = React.useMemo(() => {
     const indice = Math.floor(Math.random() * emojis.length);
     return emojis[indice];
@@ -21,7 +23,7 @@ function Header() {
         <span>{emoji}</span>
       </Welcome>
       <ButtonOut>
-        {pathname === '/task:id' && (
+        {pathname === `/task/${id}` && (
           <button onClick={() => navigate('/login')}>
             <img src={out} alt="Exit button" />
           </button>
@@ -30,5 +32,9 @@ function Header() {
     </Container>
   );
 }
+
+Header.propTypes = {
+  id: PropTypes.string,
+};
 
 export default Header;
